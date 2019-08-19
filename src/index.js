c9851e7e52165ca27ef539c94b23f07ec3fa8e26
Xujuan <767196201@qq.com>
2019-08-19 18:51:12 +0800

    feat: 根据jsx语法编写

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// APP是一个返回Dome对象的函数时
+ReactDOM.render(<App/>, document.getElementById('root'));
+// APP 是dome对象时
+// ReactDOM.render(App, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
